e127fcd8cebd4661942760522423e039d2df4f24
lperaza2 <lperaza2@stu.southtexascollege.edu>
2026-02-04 19:38:35 -0600

    added sections

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,46 @@
 <script setup>
   import Header from './components/Header.vue'
-    import Card from './components/Card.vue'
+  import Card from './components/Card.vue'
+  import Section from './components/Section.vue'
 </script>
 
 <template>
   <Header/>
+
   <Card>
+
     <form id="personForm" class="rowForm">
         <input id="personInput" type="text" placeholder="Add person name">
         <button>Add person</button>
     </form>
+
     <form id="expenseForm" class="rowForm">
         <input id="descInput" type="text" placeholder="Expense Description">
         <input id="amountInput" type="number" placeholder="Amount">
         <select id="paidBySelect"></select>
         <button>Add Expense</button>
     </form>
-    <section>
-        <h3>People </h3>
-        <ul id="peopleList" class="list"></ul>
-    </section>
-    <section>
-        <h3>Expenses</h3>
+
+    <Section title="People">
+      <ul id="peopleList" class="list"></ul>
+    </Section>
+    <Section title="Expenses">
         <ul id="expenseList" class="list"></ul>
-    </section>
-    <section>
-        <h3>Total</h3>
+    </Section>
+
+    <Section title="Total">
         <p>
             Total Spent: <strong id="totalSpent">$0.00</strong><br/>
             Split Per Person: <strong id="splitAmount">$0.00</strong>
         </p>
-    </section>
-    <section class="summary">
-        <h3>Summary</h3>
+    </Section>
+
+    <Section title="Summary">
         <ul id="summaryList" class="list"></ul>
-    </section>
+    </Section>
+
   </Card>
+
 </template>
 
 <style scoped>
